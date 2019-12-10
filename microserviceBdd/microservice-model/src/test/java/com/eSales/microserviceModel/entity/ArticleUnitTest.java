19055fcfc0491bbdf0c520a4dd6f8daba4f96c81
nicowtt <nicobod31@gmail.com>
2019-12-10 16:14:53 +0100

    module model: - Unit test Date -> null

--- microserviceBdd/microservice-model/src/test/java/com/eSales/microserviceModel/entity/ArticleUnitTest.java
+++ microserviceBdd/microservice-model/src/test/java/com/eSales/microserviceModel/entity/ArticleUnitTest.java
@@ -1,49 +1,33 @@
 package com.eSales.microserviceModel.entity;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
 
 @SpringBootTest
 public class ArticleUnitTest {
 
-    static private final Log logger = LogFactory.getLog(ArticleUnitTest.class);
+
 
     @Test
     public void testToString() {
         Article article = new Article();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
-//        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-        String todayDateString = "2019-12-09 17:31:25";
 
         article.setId(0);
         article.setCategory("vêtement");
         article.setType("jeans");
         article.setSaleNumber(1);
         article.setPrice(10);
-        try {
-            Date todayDate = formatter.parse(todayDateString);
-            article.setDateRecord(todayDate);
-        } catch (ParseException e) {
-            logger.info("fail to convert string on date");
-        }
+        article.setDateRecord(null);
         article.setValidateToSell(false);
         article.setStolen(false);
         article.setReturnOwner(false);
 
         System.out.println(article.toString());
-        Assert.assertEquals("Article{id=0, category='vêtement', type='jeans', saleNumber=1, price=10.0," +
-                        " dateRecord=Wed Jan 09 17:31:25 CET 2019, isValidateToSell=false, isSold=false, isStolen=false," +
-                        " isReturnOwner=false, user=null, sale=null, clothe=null, toy=null, book=null, object=null}"
+        Assert.assertEquals("Article{id=0, category='vêtement', type='jeans', saleNumber=1, price=10.0, " +
+                        "dateRecord=null, isValidateToSell=false, isSold=false, isStolen=false, isReturnOwner=false, " +
+                        "user=null, sale=null, clothe=null, toy=null, book=null, object=null}"
                 , article.toString());
     }
 }
--- microserviceBdd/microservice-model/src/test/java/com/eSales/microserviceModel/entity/SaleUnitTest.java
+++ microserviceBdd/microservice-model/src/test/java/com/eSales/microserviceModel/entity/SaleUnitTest.java
@@ -1,43 +1,25 @@
 package com.eSales.microserviceModel.entity;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+
 
 @SpringBootTest
 public class SaleUnitTest {
 
-    static private final Log logger = LogFactory.getLog(SaleUnitTest.class);
-
     @Test
     public void testToString() {
         Sale sale = new Sale();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
-//        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-        String todayDateString = "2019-12-10 14:48:25";
-        String oneWeekDateString = "2019-12-17 14:48:25";
 
         sale.setType("Bourse de printemps");
         sale.setDescription("Vêtements");
+        sale.setDateEnd(null);
+        sale.setDateBegin(null);
 
-        try {
-            Date todayDate = formatter.parse(todayDateString);
-            Date oneWeekDate = formatter.parse(oneWeekDateString);
-            sale.setDateBegin(todayDate);
-            sale.setDateEnd(oneWeekDate);
-        } catch (ParseException e) {
-            logger.info("fail to convert string on date");
-        }
-
-        Assert.assertEquals("Sale{id=0, type='Bourse de printemps', description='Vêtements', " +
-                "dateBegin=Thu Jan 10 14:48:25 CET 2019, dateEnd=Thu Jan 17 14:48:25 CET 2019, address=null}"
+        Assert.assertEquals("Sale{id=0, type='Bourse de printemps', description='Vêtements', dateBegin=null, " +
+                        "dateEnd=null, address=null}"
                 , sale.toString());
 
     }
